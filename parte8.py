a0ef2af97ea7a4e80550a6c1d4d5b9c4733afc5a
Johann <jgcamiloaga@gmail.com>
2023-11-14 16:54:19 -0500

    Fix some issue
    CLAVESECRETA

---
+++ parte8.py
@@ -0,0 +1,22 @@
+#Con un argumento
+for i in range(10):
+    print("El valor de i es", i)
+#El ultimo número será 10-1 = 9
+
+
+#Con dos argumentos
+for i in range(2, 8):
+    print("El valor de i es", i)
+#El primer número será 2
+#EL último número será 8-1 = 7
+
+
+#Con 3 argumentos
+for i in range(2, 8, 3):
+    print("El valor de i es", i)
+#El primer número será 2
+#Los números irán aumentando de 3 en 3
+#Es decir, 2+3 = 5, 5+3 = 8
+#Se imprimirán los números 2 y 5, pero no el 8, ya que es igual al límite.
+#El último número será 5.
+
---
+++ parte9.py
@@ -0,0 +1,8 @@
+import time
+
+for i in range(1,6):
+    print(i,"Mississippi")
+    time.sleep(1)
+    #tim.sleep(1) espera un segundo antes dar la siguiente vuelta
+
+print("!Listo o no, ahí voy!")
