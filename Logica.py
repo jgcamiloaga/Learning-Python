741433822bfacafc70d4ac1282aff0febf19a78a
Johann <jgcamiloaga@gmail.com>
2023-11-21 15:20:17 -0500

    Learning Python

---
+++ Logica.py
@@ -0,0 +1,21 @@
+#Lógica de computadoras
+#and (y)
+#En and tienen que cumplirse todad las condiciones
+
+#Mientras que en
+#or (o)
+#Solo basta 1 para que la condición sea valida
+
+#not
+#Convierte la verdad en falso y el falso en la verdad
+
+# Ejemplo 1:
+var = 0
+print(var > 0)
+print(not (var <= 0))
+
+
+# Ejemplo 2:
+print(var != 0)
+print(not (var == 0))
+
